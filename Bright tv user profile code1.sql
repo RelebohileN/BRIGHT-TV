f94a66e2cf6f7016b213e93ea36ab9e7c91fc0db
RelebohileN <relebohilentsane11@gmail.com>
2026-07-19 18:35:52 +0000

    Adding coding for bright tv

---
+++ Bright tv user profile code1.sql
@@ -0,0 +1,12 @@
+-- Databricks notebook source
+select* from brightdataset.viewers.bright_tv_dataset_userprofiles limit 100
+
+select *
+from brightdataset.viewers.viewership_info;
+
+select distinct gender from brightdataset.viewers.bright_tv_dataset_userprofiles
+
+
+
+
+
